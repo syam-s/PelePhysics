15893d2b325b1b52f05631c143f20b1e79ecf215
Lucas Esclapez <lesclapez@lbl.gov>
2020-10-06 11:21:35 -0700

    Add Lu skelettal CH4/Air mech. augmented with ions chem.

---
+++ Support/Fuego/Mechanism/Models/CH4_ion_LuSk/mechanism.h
@@ -0,0 +1,63 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+
+#if 0
+/* Elements
+0  O
+1  H
+2  C
+3  N
+4  E
+*/
+#endif
+
+/* Species */
+#define H2_ID 0
+#define H_ID 1
+#define O_ID 2
+#define O2_ID 3
+#define OH_ID 4
+#define H2O_ID 5
+#define HO2_ID 6
+#define H2O2_ID 7
+#define C_ID 8
+#define CH_ID 9
+#define CH2_ID 10
+#define CH2S_ID 11
+#define CH3_ID 12
+#define CH4_ID 13
+#define CO_ID 14
+#define CO2_ID 15
+#define HCO_ID 16
+#define CH2O_ID 17
+#define CH2OH_ID 18
+#define CH3O_ID 19
+#define CH3OH_ID 20
+#define C2H2_ID 21
+#define C2H3_ID 22
+#define C2H4_ID 23
+#define C2H5_ID 24
+#define C2H6_ID 25
+#define HCCO_ID 26
+#define CH2CO_ID 27
+#define CH2CHO_ID 28
+#define N2_ID 29
+#define H3Op_ID 30
+#define HCOp_ID 31
+#define C2H3Op_ID 32
+#define CH5Op_ID 33
+#define OHn_ID 34
+#define On_ID 35
+#define O2n_ID 36
+#define CO3n_ID 37
+#define HCO3n_ID 38
+#define E_ID 39
+
+#define NUM_ELEMENTS 5
+#define NUM_SPECIES 40
+#define NUM_REACTIONS 246
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
